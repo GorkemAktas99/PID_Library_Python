9a6e7b4816efbb022a3ac5797d3f483dc3f40f21
GorkemAktas99 <72526481+GorkemAktas99@users.noreply.github.com>
2022-02-23 16:12:16 +0300

    Update pid_lib.py

--- pid_lib.py
+++ pid_lib.py
@@ -1,19 +1,22 @@
 class PID:
+
     def __init__(self,kp,ki,kd,stepsize):
         self.kp = kp
         self.ki = ki
         self.kd = kd
         self.stepsize = stepsize
+
     def Derivative(self,ek,ek_1):
         self.ek = ek
         self.ek_1 = ek_1
 
-        return (self.ek-self.ek_1)/self.stepsize
+        return ( self.ek - self.ek_1 ) / self.stepsize
+
     def Integral(self,ek,ek_1):
         self.ek = ek
         self.ek_1 = ek_1
 
-        return (self.ek + self.ek_1) * (self.stepsize / 2)
+        return ( self.ek + self.ek_1 ) * ( self.stepsize / 2 )
     def PID_Function(self,r,y):
         self.ek_1 = 0
         integralSum = 0
